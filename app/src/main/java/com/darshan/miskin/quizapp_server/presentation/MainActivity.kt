6760f49898727a7dfd2509b8497c889bdef4073f
Darshan Miskin <28859845+darshan-miskin@users.noreply.github.com>
2026-06-22 14:39:55 +0530

    update ui

--- app/src/main/java/com/darshan/miskin/quizapp_server/presentation/MainActivity.kt
+++ app/src/main/java/com/darshan/miskin/quizapp_server/presentation/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.darshan.miskin.quizapp_server.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -21,16 +20,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.lifecycleScope
-import com.darshan.miskin.quizapp_server.QuizData
 import com.darshan.miskin.quizapp_server.R
-import com.darshan.miskin.quizapp_server.data.QuizRepository
-import com.darshan.miskin.quizapp_server.data.state.ResponseState
 import com.darshan.miskin.quizapp_server.presentation.ui.theme.QuizApp_ServerTheme
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
