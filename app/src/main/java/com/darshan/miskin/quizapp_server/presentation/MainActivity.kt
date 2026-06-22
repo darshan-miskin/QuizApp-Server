d51d33d76030420bcba87df9fae1e4d4df43e6e9
Darshan Miskin <28859845+darshan-miskin@users.noreply.github.com>
2026-06-22 14:39:20 +0530

    update ui

--- app/src/main/java/com/darshan/miskin/quizapp_server/presentation/MainActivity.kt
+++ app/src/main/java/com/darshan/miskin/quizapp_server/presentation/MainActivity.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -15,9 +17,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.darshan.miskin.quizapp_server.QuizData
+import com.darshan.miskin.quizapp_server.R
 import com.darshan.miskin.quizapp_server.data.QuizRepository
 import com.darshan.miskin.quizapp_server.data.state.ResponseState
 import com.darshan.miskin.quizapp_server.presentation.ui.theme.QuizApp_ServerTheme
@@ -47,27 +53,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                modifier = modifier.weight(1f),
-                text = "This is a Server app, for the Quiz Client App!"
-            )
-            Text(
-                "Server is Ready! Open client app and click Start Quiz",
-                style = MaterialTheme.typography.titleMedium
-            )
-        }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            stringResource(R.string.server_is_ready),
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text(
+            text = stringResource(R.string.you_can_close_this_app),
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center
+        )
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuizApp_ServerTheme {
-        Greeting()
+
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Greeting()
+        }
     }
 }
